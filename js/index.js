33fc8f1e149236af3b0fe938f49739187afd9a37
mcjauregui96 <mcotrina171@gmail.com>
2025-05-19 01:07:02 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,33 +25,6 @@ for (let i = 0; i < skills.length; i++) {
   skillsList.appendChild(skill);
 }
 
-//Create a footer element
-
-const footer = document.createElement("footer");
-footer.className = "footer";
-footer.setAttribute("id", "myfooter");
-document.body.appendChild(footer);
-
-// Get current year
-const today = new Date();
-const thisYear = today.getFullYear();
-const newFooter = document.querySelector("footer");
-const copyright = document.createElement("p");
-var copyrightSymbol = `\u00A9`;
-copyright.innerHTML = `${copyrightSymbol}Mario Cotrina ${thisYear}`;
-footer.appendChild(copyright);
-
-// Add to your Skills Section
-const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop"];
-const skillsSection = document.querySelector("#skills");
-const skillsList = skillsSection.getElementsByTagName("ul")[0];
-
-for (let i = 0; i < skills.length; i++) {
-  const skill = document.createElement("LI");
-  skill.innerText = skills[i];
-  skillsList.appendChild(skill);
-}
-
 
 
 
