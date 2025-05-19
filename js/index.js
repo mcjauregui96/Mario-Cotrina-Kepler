533b231131a419dff032b0c20dfa6b4a25f63c66
Mario Cotrina <mcotrina171@gmail.com>
2025-05-19 01:56:37 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -25,32 +25,65 @@ for (let i = 0; i < skills.length; i++) {
   skillsList.appendChild(skill);
 }
 
-//Create a footer element
+// Handle form submission
+const messageForm= document.querySelector('form[name="leave_message"]')
+messageForm.addEventListener("submit", function(event){
+  event.preventDefault();
+// Get data
+  const usersName= event.target.usersName.value;
+  const usersEmail= event.target.usersEmail.value;
+  const usersMessage= event.target.usersMessage.value;
+  console.log('Name:',usersName);
+  console.log('Email:',usersEmail);
+  console.log('Message:',usersMessage);
 
-const footer = document.createElement("footer");
-footer.className = "footer";
-footer.setAttribute("id", "myfooter");
-document.body.appendChild(footer);
+  //Display message
+  const messageSection= document.querySelector('#messages');
+  const messageList= messageSection.querySelector('ul');
+  const newMessage= document.createElement('li');
+  newMessage.innerHTML= `<a href= "mailto:${usersEmail}">${usersName}</a>\n
+    <span>${usersMessage}</span>`;
 
-// Get current year
-const today = new Date();
-const thisYear = today.getFullYear();
-const newFooter = document.querySelector("footer");
-const copyright = document.createElement("p");
-var copyrightSymbol = `\u00A9`;
-copyright.innerHTML = `${copyrightSymbol}Mario Cotrina ${thisYear}`;
-footer.appendChild(copyright);
+  // Create remove button
+  const removeButton= document.createElement('button');
+  removeButton.innerText='remove';
+  removeButton.type='button';
+  removeButton.addEventListener('click', function(e){
+    e.target.parentNode.remove();
+    toggleMessageSection();
+  });
 
-// Add to your Skills Section
-const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop"];
-const skillsSection = document.querySelector("#skills");
-const skillsList = skillsSection.getElementsByTagName("ul")[0];
+  //Create edit button
+  const editButton= document.createElement('button');
+  editButton.innerText='edit';
+  editButton.type='button';
+  editButton.addEventListener('click', function(edit){
+    const span=edit.target.parentNode.querySelector('span');
+    const newText= prompt('Edit your message:', span.innerText);
+    if (newText) span.innerText= newText;
+  });
 
-for (let i = 0; i < skills.length; i++) {
-  const skill = document.createElement("LI");
-  skill.innerText = skills[i];
-  skillsList.appendChild(skill);
-}
+  //Append buttons and message
+  newMessage.appendChild(removeButton);
+  newMessage.appendChild(editButton);
+  messageList.appendChild(newMessage);
+
+  
+//Reset form
+  event.target.reset();
+
+  //Hide messages section
+  function toggleMessageSection(){
+    const messageSection= document.querySelector('#messages');
+    const messageList= messageSection.querySelector('ul');
+    if (messageList.children.length === 0){
+      messageSection.style.display= 'none';
+    } else{
+      messageSection.style.display='block';
+    }
+  }
+  toggleMessageSection();
+})
 
 
 
